68dd25f41c9e9c3e78f6251577b51ec7fe3da9ac
mahendra-gehlot <connect.mge@gmail.com>
2022-11-23 20:04:35 +0530

    normalization added in predict

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -5,16 +5,18 @@ import numpy as np
 import math
 import itertools
 import sys
+from tqdm import tqdm
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
 from torch.autograd import Variable
+import torchvision.transforms as transforms
 
 from model import *
 from make_dataset import ImageDataset
 
 cuda = torch.cuda.is_available()
 
-hr_shape = (512, 512)
+hr_shape = (720, 720)
 
 # Initialize generator and discriminator
 generator = GeneratorResNet()
@@ -39,14 +41,23 @@ if cuda:
 Tensor = torch.cuda.FloatTensor if cuda else torch.Tensor
 
 dataloader = DataLoader(
-    ImageDataset("data/video/data", hr_shape=hr_shape),
+    ImageDataset("data/sub_sample/", hr_shape=hr_shape),
     batch_size=1,
     shuffle=False,
     num_workers=2,
 )
 
+mean = np.array([0.485, 0.456, 0.406])
+std = np.array([0.229, 0.224, 0.225])
 
-for i, images in enumerate(dataloader):
+inv_normalize = transforms.Normalize(
+   mean= [-m/s for m, s in zip(mean, std)],
+   std= [1/s for s in std]
+)
+
+
+print('Hawk-Eye is on work......')
+for i, images in enumerate(tqdm(dataloader)):
     # Configure model input
     imgs_lr = Variable(images["lr"].type(Tensor))
     imgs_hr = Variable(images["hr"].type(Tensor))
@@ -55,4 +66,6 @@ for i, images in enumerate(dataloader):
     # Generate a high resolution image from low resolution input
     gen_hr = generator(imgs_lr)
     
-    save_image(gen_hr,f"reports/predict/{i}.png")
+    save_image(inv_normalize(imgs_lr),f"reports/predict/lr_{i}.png")
+    save_image(inv_normalize(imgs_hr),f"reports/predict/hr_{i}.png")
+    save_image(inv_normalize(gen_hr),f"reports/predict/ghr_{i}.png")
