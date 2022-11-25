7b5a21fdeebd0224b15a80c0381f095cac51c014
mahendra-gehlot <connect.mge@gmail.com>
2022-11-25 13:22:31 +0530

    ssim-amended

---
+++ src/utility/make_video.py
@@ -0,0 +1,30 @@
+import cv2
+import os
+
+cam = cv2.VideoCapture('data/videos/videoplayback.mp4')
+
+# frame
+currentframe = 0
+
+while (True):
+
+    # reading from frame
+    ret, frame = cam.read()
+
+    if ret:
+        # if video is still left continue creating images
+        name = 'report/video_frames' + str(currentframe) + '.jpg'
+        print('Creating...' + name)
+
+        # writing the extracted images
+        cv2.imwrite(name, frame)
+
+        # increasing counter so that it will
+        # show how many frames are created
+        currentframe += 1
+    else:
+        break
+
+# Release all space and windows once done
+cam.release()
+
