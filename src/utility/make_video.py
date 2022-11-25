c56585df9bdcff5999ad21873408b24c05152f30
mahendra-gehlot <connect.mge@gmail.com>
2022-11-25 15:57:37 +0530

    video-maker-added

--- src/utility/make_video.py
+++ src/utility/make_video.py
@@ -1,16 +1,17 @@
 import glob
 import cv2
 
+size = None
 img_array = []
-all_files = sorted(glob.glob("reports/video_frames/*.jpg"))
+all_files = sorted(glob.glob("reports/video_frames/*.png"))
 for filename in all_files:
     img = cv2.imread(filename)
     height, width, layers = img.shape
     size = (width, height)
     img_array.append(img)
-
-out = cv2.VideoWriter('reports/sr_videos.mp4', cv2.VideoWriter_fourcc(*'DIVX'),
-                      15, size)
+fps = 30
+out = cv2.VideoWriter('reports/sr_videos.mp4', cv2.VideoWriter_fourcc(*'mpeg'),
+                      fps, size)
 
 for i in range(len(img_array)):
     out.write(img_array[i])
